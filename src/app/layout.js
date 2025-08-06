5d61b0b5c640757eab85f7592d4b10243d76ef37
Ammar <ammaruchia29@gmail.com>
2025-08-06 21:29:11 +0300

    added necessary styles and typefaces

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,46 @@
 import "./globals.css";
+import localFont from "next/font/local";
+import { Poppins } from "next/font/google";
+
+// Configure Poppins font
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  style: ["normal", "italic"],
+  variable: "--font-poppins",
+});
+
+// Configure Sharpie fonts
+const sharpie = localFont({
+  src: [
+    {
+      path: "../../public/fonts/Sharpie-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Sharpie-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Sharpie-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Sharpie-Extrabold.woff2",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Sharpie-Black.woff2",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+  variable: "--font-sharpie",
+});
 
 export const metadata = {
   title: "We Leauge",
@@ -8,7 +50,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        style={{ fontFamily: "var(--font-poppins), sans-serif" }}
+        className={`${poppins.variable} ${sharpie.variable}`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,3 @@
-import Link from "next/link";
-
 function page() {
   return <div></div>;
 }
