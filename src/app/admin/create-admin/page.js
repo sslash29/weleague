bd20a1b946fa0a908fae68604416f527bbee05be
Ammar <ammaruchia29@gmail.com>
2025-08-08 17:35:41 +0300

    finished the createAdmin

--- src/app/admin/create-admin/page.js
+++ src/app/admin/create-admin/page.js
@@ -2,9 +2,11 @@ import CreateAdminForm from "@/components/CreateAdminForm";
 
 function page() {
   return (
-    <div className="flex flex-col">
-      <h2>Set Up a new admin</h2>
-      <CreateAdminForm />
+    <div className="flex items-center h-dvh justify-center ">
+      <div className="flex flex-col gap-2.5">
+        <h2 className="text-5xl font-bold w-[300px]">Create New Admin</h2>
+        <CreateAdminForm />
+      </div>
     </div>
   );
 }
