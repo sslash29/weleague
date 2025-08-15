8f782a224dede2d29d73c1ea53d3d568945a4427
Ammar <ammaruchia29@gmail.com>
2025-08-15 11:21:57 +0300

    feat: update when click on logo it reutrns to homepage

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,14 +1,18 @@
 "use client";
 
+import Link from "next/link";
+
 function Logo() {
   return (
     <div>
-      <h2
-        style={{ fontFamily: "var(--font-sharpie), sans-serif" }}
-        className="text-violet-normal font-bold text-3xl"
-      >
-        weleague
-      </h2>
+      <Link href="/">
+        <h2
+          style={{ fontFamily: "var(--font-sharpie), sans-serif" }}
+          className="text-violet-normal font-bold text-3xl"
+        >
+          weleague
+        </h2>
+      </Link>
     </div>
   );
 }
