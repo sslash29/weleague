d5a08985eeccafac030fb801bec5c76639b65655
Ammar <ammaruchia29@gmail.com>
2025-08-21 10:26:02 +0300

    fix: now the correct all_time_data and weekly_points are being submitted to the team table

--- src/app/moderator/add-player-data/[playerId]/[teamId]/page.js
+++ src/app/moderator/add-player-data/[playerId]/[teamId]/page.js
@@ -1,11 +1,12 @@
 import AddPlayerDataForm from "@/components/Moderator/PlayerData/AddPlayerDataForm";
+import { getPlayerTeam } from "@/services/services";
 
 async function Page({ params }) {
-  const { id } = (await params) || {};
+  const { playerId, teamId } = (await params) || {};
 
   return (
     <div>
-      <AddPlayerDataForm playerId={id} />
+      <AddPlayerDataForm playerId={playerId} teamId={teamId} />
     </div>
   );
 }
