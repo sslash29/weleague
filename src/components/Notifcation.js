7f4ab2e54508686bddacfc4920f0d882dea76dea
Ammar <ammaruchia29@gmail.com>
2025-08-23 11:46:42 +0300

    fix: notification message doesn't break the layout of the page

--- src/components/Notifcation.js
+++ src/components/Notifcation.js
@@ -1,6 +1,6 @@
 function Notifcation({ errorMsg }) {
   return (
-    <div className="h-[55vh] w-[calc(100vw-32px)] relative flex items-end justify-between">
+    <div className="h-full w-full relative flex items-end justify-between">
       <div className=" p-3  rounded-md flex items-center gap-3 shadow font-semibold text-lgbg-white ">
         <img src="/Error.svg" alt="Error" />
         <p>{errorMsg}</p>
